92cb2d8a64daed956d4183118ecd4bb8631fe348
naatti16 <148633784+anttitulenheimo@users.noreply.github.com>
2025-10-21 16:59:00 +0300

    Add avatar to settings

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Box from '@mui/material/Box'
-import Typography from '@mui/material/Typography'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
@@ -9,10 +8,17 @@ import ListItem from '@mui/material/ListItem'
 import LogoutIcon from '@mui/icons-material/Logout'
 import AddBoxIcon from '@mui/icons-material/AddBox'
 import LanguageIcon from '@mui/icons-material/Language'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
+import Avatar from '@mui/material/Avatar'
+
 
 const Settings = () => {
   return (
     <Box sx={{ p: 3, minHeight: 'calc(100vh - 56px)' }}>
+        <Box>
+            <Avatar alt="Test User" sx={{ width: 100, height: 100 }}
+                    src="src/assets/aaltoAvatar.png" /> Logged in as Test User
+        </Box>
         <List>
           <ListItem disablePadding>
             <ListItemButton>
@@ -30,6 +36,12 @@ const Settings = () => {
               <ListItemText primary="Add another device" />
             </ListItemButton>
           </ListItem>
+          <ListItemButton>
+              <ListItemIcon>
+                <NotificationsActiveIcon />
+              </ListItemIcon>
+              <ListItemText primary="Reminders" />
+            </ListItemButton>
           <ListItem disablePadding>
             <ListItemButton>
               <ListItemIcon>
